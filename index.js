4a954ba95e49f64bbaaafb12900148a0850020c0
Kia King Ishii <kia.king.08@gmail.com>
2021-02-27 00:55:48 +0900

    update

--- index.js
+++ index.js
@@ -1,150 +1,142 @@
-console.log('HELLO FROM CHE TSUMI')
-console.log(process.env.TEST_NAME)
-// const RssFeedEmitter = require('rss-feed-emitter')
-// const Queue = require('queue')
-// const Github = require('./lib/github')
-// const Repo = require('./lib/repository')
-// // const Slack = require('./lib/slack')
-// const Utility = require('./lib/utility')
+const Rss = require('rss-parser')
+const Queue = require('queue')
+const Github = require('./lib/github')
+const Repo = require('./lib/repository')
+const Utility = require('./lib/utility')
 
-// let upstreamFeeder = new RssFeedEmitter()
-// let headFeeder = new RssFeedEmitter()
-// let github = new Github()
-// let q = Queue({ autostart: true, concurrency: 1 })
-// // let slack = new Slack({ token: process.env.SLACK_TOKEN })
+const remote = {
+  upstream: {
+    url: process.env.UPSTREAM_REPO_URL,
+    owner: Utility.extractRepoOwner(process.env.UPSTREAM_REPO_URL),
+    name: Utility.extractRepoName(process.env.UPSTREAM_REPO_URL),
+    defaultBranch: process.env.UPSTREAM_REPO_DEFAULT_BRANCH
+  },
+  head: {
+    url: process.env.HEAD_REPO_URL,
+    owner: Utility.extractRepoOwner(process.env.HEAD_REPO_URL),
+    name: Utility.extractRepoName(process.env.HEAD_REPO_URL),
+    defaultBranch: process.env.HEAD_REPO_DEFAULT_BRANCH
+  }
+}
 
-// let startUpTime = new Date().toISOString()
-// let timer = null
+const rss = new Rss()
+const github = new Github(process.env.GITHUB_ACCESS_TOKEN)
+const q = Queue({ autostart: true, concurrency: 1 })
 
-// let remote = {
-//   origin: {
-//     url: process.env.ORIGIN_REPO_URL,
-//     owner: Utility.extractRepoOwner(process.env.ORIGIN_REPO_URL),
-//     name: Utility.extractRepoName(process.env.ORIGIN_REPO_URL),
-//     defaultBranch: process.env.ORIGIN_REPO_DEFAULT_BRANCH,
-//   },
-//   upstream: {
-//     url: process.env.UPSTREAM_REPO_URL,
-//     owner: Utility.extractRepoOwner(process.env.UPSTREAM_REPO_URL),
-//     name: Utility.extractRepoName(process.env.UPSTREAM_REPO_URL),
-//     defaultBranch: process.env.UPSTREAM_REPO_DEFAULT_BRANCH,
-//   },
-//   head: {
-//     url: process.env.HEAD_REPO_URL,
-//     name: Utility.extractRepoName(process.env.HEAD_REPO_URL),
-//     defaultBranch: process.env.HEAD_REPO_DEFAULT_BRANCH,
-//   },
-// }
+const repo = new Repo({
+  path: 'repo',
+  remote,
+  user: {
+    name: process.env.GITHUB_USER_NAME,
+    email: process.env.GITHUB_EMAIL
+  }
+})
 
-// let repo = new Repo(
-//   {
-//     path: 'repo',
-//     remote,
-//     user: {
-//       name: process.env.USER_NAME,
-//       email: process.env.EMAIL,
-//     },
-//   }
-// )
+function setup() {
+  repo.setup()
+  processHeadFeed()
+}
 
-// const setup = () => {
-//   repo.setup()
-//   github.authenticate({ type: 'token', token: process.env.GITHUB_ACCESS_TOKEN })
-//   setupUpstreamFeeder()
-//   setupHeadFeeder()
-// }
+async function processHeadFeed() {
+  const [run, feed] = await Promise.all([
+    github.getLatestWorkflowRun(remote),
+    getFeed()
+  ])
 
-// const setupHeadFeeder = () => {
-//   headFeeder.add({
-//     url: process.env.HEAD_FEED_URL,
-//     refresh: Number(process.env.HEAD_FEED_REFRESH),
-//   })
+  const runDate = new Date(run.created_at).toISOString()
 
-//   headFeeder.on('new-item', async (item) => {
-//     Utility.log('I', `New commit on head repo: ${item.title}`)
+  feed.items.forEach(async (item) => {
+    if (runDate > item.isoDate) {
+      return
+    }
 
-//     const hash = Utility.extractBasename(item.link)
-//     // branch names consisting of 40 hex characters are not allowed
-//     const shortHash = hash.substr(0, 8)
+    Utility.log('I', `New commit on head repo: ${item.contentSnippet}`)
 
-//     if (repo.existsRemoteBranch(shortHash)) {
-//       Utility.log('W', `${item.title}: Remote branch already exists`)
-//       return
-//     }
+    const hash = Utility.extractBasename(item.link)
 
-//     const { data: result } = await github.searchIssue(remote, { hash })
-//     let issueNo = null
-//     if (result.total_count === 0) {
-//       let body = `本家のドキュメントに更新がありました:page_facing_up:\r\nOriginal:${item.link}`
-//       const { data: newIssue } = await github.createIssue(remote, { title: `[Doc]: ${Utility.removeHash(item.title)}`, body, labels: ['documentation'] })
-//       issueNo = newIssue.number
-//       Utility.log('S', `${item.title}: Issue created: ${newIssue.html_url}`)
-//     } else {
-//       issueNo = result.items[0].number
-//     }
+    // branch names consisting of 40 hex characters are not allowed
+    const shortHash = hash.substr(0, 8)
 
-//     q.push(() =>  {
-//       return new Promise(async (resolve, reject) => {
-//         try {
-//           q.stop()
+    if (repo.existsRemoteBranch(shortHash)) {
+      Utility.log('W', `${item.contentSnippet}: Remote branch already exists`)
+      return
+    }
 
-//           repo.fetchAllRemotes()
-//           repo.updateDefaultBranch()
-//           repo.deleteOldBranch(shortHash)
-//           repo.createNewBranch(shortHash)
+    const { data: result } = await github.searchIssue(remote, { hash })
 
-//           if (repo.hasConflicts('cherry-pick', hash)) {
-//             Utility.log('W', `${item.title}: Conflicts occurred. Please make a pull request by yourself`)
-//             repo.resetChanges()
-//           } else {
-//             Utility.log('S', `${item.title}: Fully merged`)
-//             repo.updateRemote(shortHash)
-//             await after(item, shortHash, issueNo)
-//           }
+    const issueNo = await getIssueNo(item, hash)
 
-//           resolve()
-//         } catch(e) {
-//           reject()
-//         } finally {
-//           q.start()
-//         }
-//       })
-//     })
-//   })
-// }
+    q.push(async () => {
+      try {
+        q.stop()
 
-// const setupUpstreamFeeder = () => {
-//   upstreamFeeder.add({
-//     url: process.env.UPSTREAM_FEED_URL,
-//     refresh: Number(process.env.UPSTREAM_FEED_REFRESH),
-//   })
+        repo.fetchHead()
+        repo.updateDefaultBranch()
+        repo.deleteOldBranch(shortHash)
+        repo.createNewBranch(shortHash)
 
-//   upstreamFeeder.on('new-item', (item) => {
-//     if (startUpTime < item.date.toISOString()) {
-//       Utility.log('I', `New commit on upstream repo: ${item.title}`)
-//       removeHeadFeeder()
-//       clearTimeout(timer)
-//       timer = setTimeout(() => setupHeadFeeder(), 300000)
-//     }
-//   })
-// }
+        if (repo.hasConflicts('cherry-pick', hash)) {
+          Utility.log('W', `${item.title}: Conflicts occurred. Please make a pull request by yourself`)
+          repo.resetChanges()
+        } else {
+          Utility.log('S', `${item.title}: Fully merged`)
+          repo.updateRemote(shortHash)
+          await after(item, shortHash, issueNo)
+        }
+      } catch (e) {
+        throw e
+      } finally {
+        q.start()
+      }
+    })
+  })
+}
 
-// const removeHeadFeeder = () => {
-//   headFeeder.off('new-item')
-//   headFeeder.destroy()
-// }
+function getFeed() {
+  const repoUrl = process.env.HEAD_REPO_URL
+  const repoBranch = process.env.HEAD_REPO_DEFAULT_BRANCH
 
-// const after = async (item, shortHash, issueNo = null) => {
-//   const body = issueNo ? `This PR resolves #${issueNo}\r\nCherry picked from ${item.link}` : `Cherry picked from ${item.link}`
-//   const { data: pullRequest } = await github.createPullRequest(remote, { title: Utility.removeHash(item.title), body, branch: shortHash })
-//   if (!pullRequest) return
-//   Utility.log('S', `Created new pull request: ${pullRequest.html_url}`)
-//   await github.assignReviewers(remote, { number: pullRequest.number, reviewers: ['re-fort', 'kazupon', 'potato4d'] })
-//   Utility.log('S', 'Assigned reviewers')
-//   await github.addLabels(remote, { number: issueNo, labels: ['assigned'] })
-//   Utility.log('S', 'Added "assigned" label to issue')
-// }
+  return rss.parseURL(`${repoUrl}/commits/${repoBranch}.atom`)
+}
 
-// process.on('unhandledRejection', err => { Utility.log('E', err) })
+async function getIssueNo(item, hash) {
+  const { data: result } = await github.searchIssue(remote, { hash })
 
-// setup()
+  if (result.total_count > 0) {
+    return result.items[0].number
+  }
+
+  const body = `New updates on head repo :page_facing_up:\r\nOriginal:${item.link}`
+
+  const { data: newIssue } = await github.createIssue(remote, {
+    title: `[Doc]: ${Utility.removeHash(item.contentSnippet)}`,
+    body,
+    labels: ['documentation']
+  })
+
+  Utility.log('S', `${item.contentSnippet}: Issue created: ${newIssue.html_url}`)
+
+  return newIssue.number
+}
+
+const after = async (item, shortHash, issueNo = null) => {
+  const body = issueNo
+    ? `resolves #${issueNo}\r\nCherry picked from ${item.link}`
+    : `Cherry picked from ${item.link}`
+
+  const { data: pullRequest } = await github.createPullRequest(remote, {
+    title: Utility.removeHash(item.title),
+    body,
+    branch: shortHash
+  })
+
+  if (!pullRequest) {
+    return
+  }
+
+  Utility.log('S', `Created new pull request: ${pullRequest.html_url}`)
+}
+
+process.on('unhandledRejection', err => { Utility.log('E', err) })
+
+setup()
