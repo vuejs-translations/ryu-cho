810ccac77a0a46d54fada60bc5621cb706c83bd9
Kia King Ishii <kia.king.08@gmail.com>
2021-02-27 20:20:55 +0900

    less log

--- src/chetsumi.ts
+++ src/chetsumi.ts
@@ -47,7 +47,6 @@ export class CheTsumi {
     const run = await this.getRun()
     const feed = await this.getFeed()
 console.log('run', run)
-console.log('feed', feed)
     for (const i in feed) {
       console.log('feed', feed[i].isoDate)
       console.log('feed', feed[i].contentSnippet)
