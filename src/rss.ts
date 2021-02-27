130ced6e76349fec362df6d3605151b62e0f66a5
Kia King Ishii <kia.king.08@gmail.com>
2021-02-27 20:09:02 +0900

    loglog

--- src/rss.ts
+++ src/rss.ts
@@ -12,9 +12,10 @@ export class Rss {
   async get(remote: Remote, from: string) {
     const url = remote.url
     const branch = remote.branch
+    console.log(`${url}/commits/${branch}.atom`)
 
     const feed = await this.api.parseURL(`${url}/commits/${branch}.atom`)
-
+console.log('feed-api', feed)
     const target = feed.items.findIndex((item) => {
       return extractBasename(item.link!) === from
     })
